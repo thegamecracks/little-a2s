d6bae3713ea5631d43c67e8bb291edf07f61caa8
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-11-05 14:33:57 -0500

    test: add A2S_INFO goldsource test

--- tests/test_events.py
+++ tests/test_events.py
@@ -1,8 +1,19 @@
-from little_a2s.events import VAC, ClientEventInfo, Environment, ServerType, Visibility
+from little_a2s.events import (
+    VAC,
+    ClientEventGoldsourceInfo,
+    ClientEventInfo,
+    Environment,
+    GoldsourceMod,
+    GoldsourceModDLL,
+    GoldsourceModType,
+    ServerType,
+    Visibility,
+)
 from little_a2s.reader import Reader
 
 from .constants import (
     A2S_INFO_COUNTERSTRIKE_SOURCE,
+    A2S_INFO_GOLDSOURCE_COUNTERSTRIKE,
     A2S_INFO_SIN1_MP,
 )
 
@@ -44,4 +55,33 @@ def test_a2s_info() -> None:
         vac=VAC.INSECURE,
         version="1.0.0.0",
         extra=None,
+    )
+
+
+def test_a2s_info_goldsource() -> None:
+    info = ClientEventGoldsourceInfo.from_reader(
+        Reader(A2S_INFO_GOLDSOURCE_COUNTERSTRIKE[5:])
+    )
+    assert info == ClientEventGoldsourceInfo(
+        address="77.111.194.110:27015",
+        name="FR - VeryGames.net - Deatmatch - only surf_ski - ngR",
+        map="surf_ski",
+        folder="cstrike",
+        game="Counter-Strike",
+        players=12,
+        max_players=18,
+        protocol=47,
+        type=ServerType.DEDICATED,
+        environment=Environment.LINUX,
+        visibility=Visibility.PUBLIC,
+        mod=GoldsourceMod(
+            link="www.counter-strike.net",
+            download_link="",
+            version=1,
+            size=184000000,
+            type=GoldsourceModType.SINGLE_AND_MULTIPLAYER,
+            dll=GoldsourceModDLL.EXTENSION,
+        ),
+        vac=VAC.SECURE,
+        bots=0,
     )
