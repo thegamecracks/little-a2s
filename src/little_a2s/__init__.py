0c299f9a332edaecc8bb8e3903148df4fb4959f8
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-11-05 09:44:07 -0500

    feat: add header dataclasses

---
+++ src/little_a2s/__init__.py
@@ -0,0 +1,7 @@
+from .headers import (
+    CompressionHeader as CompressionHeader,
+    Header as Header,
+    HeaderType as HeaderType,
+    MultiGoldsourceHeader as MultiGoldsourceHeader,
+    MultiSourceHeader as MultiSourceHeader,
+)
---
+++ src/little_a2s/headers.py
@@ -0,0 +1,45 @@
+# https://developer.valvesoftware.com/wiki/Server_queries
+from __future__ import annotations
+
+from dataclasses import dataclass
+from enum import IntEnum
+
+
+class HeaderType(IntEnum):
+    SIMPLE = -1
+    MULTI = -2
+
+
+@dataclass(kw_only=True)
+class Header:
+    """An A2S response header."""
+
+    type: HeaderType
+
+
+@dataclass(kw_only=True)
+class MultiGoldsourceHeader(Header):
+    """A multi-packet response header for Goldsource games."""
+
+    id: int
+    current: int
+    total: int
+
+
+@dataclass(kw_only=True)
+class MultiSourceHeader(Header):
+    """A multi-packet response header for Source games."""
+
+    id: int
+    current: int
+    total: int
+    size: int  # Some games omit this field!
+    compressed: CompressionHeader | None
+
+
+@dataclass(kw_only=True)
+class CompressionHeader:
+    """The compression header for Source games. Mostly present in ~2006-era engines."""
+
+    size: int
+    checksum: int
