48976da99478a4dfc4237c0fb03190c967f13d04
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-11-05 22:06:17 -0500

    docs: set up extensions and theme

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,8 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+from sphinx.application import Sphinx
+
 import little_a2s
 
 project = "little-a2s"
@@ -16,14 +18,47 @@ release = little_a2s.__version__
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx_autodoc_typehints",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#configuration
+autodoc_default_options = {
+    "inherited-members": True,
+    "show-inheritance": True,
+    "members": True,
+}
+autodoc_member_order = "groupwise"
+
+# https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
+html_theme_options = {
+    "repository_url": "https://github.com/thegamecracks/little-a2s",
+    "repository_branch": "main",
+    "use_repository_button": True,
+    "use_download_button": False,
+    "use_fullscreen_button": False,
+}
+
+
+def skip_init(app: Sphinx, what: str, name: str, obj, skip: bool, options):
+    # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#event-autodoc-skip-member
+    if what == "method" and name == "__init__":
+        return True
+
+
+def setup(app: Sphinx):
+    app.connect("autodoc-skip-member", skip_init)
