f748b0a62d26cd34fb6c01a5b5f523dbb2a05ec2
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-11-05 23:04:58 -0500

    docs: add docstrings for header classes

--- src/little_a2s/headers.py
+++ src/little_a2s/headers.py
@@ -7,7 +7,9 @@ from little_a2s.enums import _EnumReprMixin
 
 class HeaderType(_EnumReprMixin, IntEnum):
     SIMPLE = -1
+    """Means the response isn't split."""
     MULTI = -2
+    """Means the response is split."""
 
 
 @dataclass(kw_only=True)
@@ -15,7 +17,9 @@ class Compression:
     """The compression header for Source games. Mostly present in ~2006-era engines."""
 
     size: int
+    """Size of the whole response once it is decompressed."""
     checksum: int
+    """CRC32 checksum of uncompressed response."""
 
 
 @dataclass(kw_only=True)
@@ -33,10 +37,15 @@ class MultiHeader(Header):
     """A multi-packet A2S response header for Source games."""
 
     id: int
+    """Unique number assigned by server per answer."""
     current: int
+    """The number of the packet."""
     total: int
+    """The total number of packets in the response."""
     size: int  # Some games omit this field!
+    """The maximum size of packet before packet switching occurs, usually 1248 bytes."""
     compressed: Compression | None
+    """An optional compression header. Mostly present in ~2006-era engines."""
 
 
 @dataclass(kw_only=True)
@@ -44,5 +53,8 @@ class MultiGoldsourceHeader(Header):
     """A multi-packet A2S response header for Goldsource games."""
 
     id: int
+    """Unique number assigned by server per answer."""
     current: int
+    """The number of the packet."""
     total: int
+    """The total number of packets in the response."""
