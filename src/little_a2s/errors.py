35d97be935445779b39496243bf0df4275f43cf4
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-11-22 09:53:49 -0500

    docs: describe how PayloadError is raised

--- src/little_a2s/errors.py
+++ src/little_a2s/errors.py
@@ -9,6 +9,9 @@ class Error(Exception):
 class PayloadError(ValueError, Error):
     """A malformed A2S packet was received.
 
+    This is raised by :class:`A2SClientProtocol` and propagated up to
+    the :class:`A2S` and :class:`AsyncA2S` clients.
+
     .. versionadded:: 0.6.0
 
     """
